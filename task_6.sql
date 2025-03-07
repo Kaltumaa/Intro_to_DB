56745f1e5cda268cc821c4f66e88d253dc65cb00
Kaltuma Omar <118067808+Kaltumaa@users.noreply.github.com>
2025-03-08 01:26:30 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
--- Use the correct database
+-- task_6.sql
+
 USE alx_book_store;
 
--- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
